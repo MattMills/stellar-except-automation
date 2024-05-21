dcae6c305238fe66b46d369d0a74de59b4cabaf2
Matt Mills <mmills@2bn.net>
2024-05-20 21:37:37 -0600

    changes to stellar-env to support cloud stuff

--- stellar-env.sh
+++ stellar-env.sh
@@ -1,6 +1,13 @@
-export GHIDRA_PATH="/zpool0/share/ghidra_10.1.5_PUBLIC"
+export GHIDRA_PATH="/zpool0/share/ghidra_11.0_PUBLIC"
 export GHIDRA_PROJECTS="/zpool0/share/ghidra_projects/"
 export GHIDRA_SCRIPTS="/zpool0/share/stellar-except-automation"
+export GHIDRA_URL="ghidra://127.0.0.1"
+
+export SCRIPT_URL="https://exceptions.dev.stellarstellaris.com/cloud_automation/ghidra_prescript.py"
+export BASE_URL="https://exceptions.dev.stellarstellaris.com/cloud_automation"
+export WEB_DIR="/var/www/html/stellar-except/cloud_automation/"
+
 export STELLAR_BACKUPS_GOG="/zpool0/share/stellaris_backups_gog"
-export STELLAR_BACKUPS_STEAM="/zpool0/share/stellaris_backups_steam"
+export STELLAR_BACKUPS_STEAM="/zpool0/share/stellaris_backups_steam/by_version"
 export WINDOWS_USER_HOST="Matt@10.5.10.2"
+export GOOGLE_APPLICATION_CREDENTIALS="/root/stellarstellaris_ghidra_gcp_credentials.json"
